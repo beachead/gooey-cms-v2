28adb75419549deb84cc830e17c41cfe142e158d
cadams500 <cadams@prayer-warrior.net>
2010-08-01 21:55:10 -0500

    Added ability to dynamically rewrite all <img> tags to the blob storage location.
    Fixed bug in image markup tag introduced when adding "class" support.

--- gooey-cms/gooeycms.business/Markup/Standard/ImageTagFormatter.cs
+++ gooey-cms/gooeycms.business/Markup/Standard/ImageTagFormatter.cs
@@ -7,13 +7,31 @@ namespace Beachead.Core.Markup.Standard
 {
     public class ImageTagFormatter : BaseFormatter
     {
-        private static Regex Image = new Regex(@"\[\[image:\s*~(.*?)(\|(.*?))? (class=(.*?))?\]\]", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.IgnoreCase);
-
+        private static Regex Image = new Regex(@"\[\[image:\s*~(.*?)(\|(.*?))?\s*(class=(.*?))?\]\]", RegexOptions.Compiled | RegexOptions.Singleline | RegexOptions.IgnoreCase);
+        private static Regex ImageHtml = new Regex(@"<img\s+.*?src\=([\x27\x22])?(?<Url>[^\x27\x22|\s|\\|>]*)(?=[\x27\x22])?.*?>",RegexOptions.Compiled | RegexOptions.IgnoreCase);
         #region IMarkupFormatter Members
 
         public override StringBuilder Convert(StringBuilder markup)
         {
             String container = CurrentSite.GetContainerUrl(SiteHelper.ImagesDirectoryKey);
+
+            //rewrite all <img> urls to the container
+            Match match = ImageHtml.Match(markup.ToString());
+            while (match.Success)
+            {
+                String origurl = match.Groups["Url"].Value;
+                String fullimagetag = match.Groups[0].Value;
+                
+                String filename = origurl;
+                int pos = origurl.LastIndexOf("/");
+                if (pos >= 0)
+                    filename = origurl.Substring(pos + 1);
+                String imagetag = fullimagetag.Replace(origurl, container + "/" + filename);
+
+                markup = new StringBuilder(ImageHtml.Replace(markup.ToString(), imagetag, 1));
+                match = match.NextMatch();
+            }
+
             markup = new StringBuilder(Image.Replace(markup.ToString(),@"<img src=""" + container + @"$1"" alt=""$3"" class=""$5"" />"));
 
             return markup;
