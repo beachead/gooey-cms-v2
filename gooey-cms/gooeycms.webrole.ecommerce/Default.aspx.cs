1731acea07364ca007ca17cf9e7af35f4c9c1486
cadams500 <cadams@prayer-warrior.net>
2010-09-26 16:03:51 -0500

    Modified the free-trial callout to actually have the correct expiration date

--- gooey-cms/gooeycms.webrole.ecommerce/Default.aspx.cs
+++ gooey-cms/gooeycms.webrole.ecommerce/Default.aspx.cs
@@ -16,6 +16,7 @@ namespace Gooeycms.Webrole.Ecommerce.store
     {   
         protected void Page_Load(object sender, EventArgs e)
         {
+
             if (!Page.IsPostBack)
             {
                 DoDataBind();
