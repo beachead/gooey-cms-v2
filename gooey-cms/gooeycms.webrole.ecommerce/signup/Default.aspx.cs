929c890e165b16107eab27d1bff7b655bcf914fe
cadams500 <cadams@prayer-warrior.net>
2010-09-26 16:04:03 -0500

    Modified the free-trial callout to actually have the correct expiration date

--- gooey-cms/gooeycms.webrole.ecommerce/signup/Default.aspx.cs
+++ gooey-cms/gooeycms.webrole.ecommerce/signup/Default.aspx.cs
@@ -14,6 +14,7 @@ namespace Gooeycms.Webrole.Ecommerce
 {
     public partial class Signup : System.Web.UI.Page
     {
+        protected String TrialExpires = DateTime.Now.AddDays(30).ToString("MMMM dd,yyyy");
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!Page.IsPostBack)
