2eaaf482ca6b293929ab762fff782002c95e133b
Carlos Mossman <carlosmossman@gmail.com>
2010-10-06 13:20:31 -0400

    content placeholders for LocalCSS and LocalJS in master; moved local styles into individual pages to prevent layout breakage on unrelated pages; packaged up features on store as showable/hideable divs, triggered on hover of features link; overflow:auto on #content to force footer to bottom and assure page bg paints; misc style revisions

--- gooey-cms/gooeycms.webrole.ecommerce/ContentPage.Master.designer.cs
+++ gooey-cms/gooeycms.webrole.ecommerce/ContentPage.Master.designer.cs
@@ -13,6 +13,24 @@ namespace Gooeycms.Webrole.Ecommerce {
     public partial class ContentPage {
         
         /// <summary>
+        /// localCSS control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder localCSS;
+        
+        /// <summary>
+        /// localJS control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder localJS;
+        
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
--- gooey-cms/gooeycms.webrole.ecommerce/js/store.js
+++ gooey-cms/gooeycms.webrole.ecommerce/js/store.js
@@ -1,38 +1,49 @@
-$(function(){
+$(function () {
 
-	var jPriceList = $('<ul/>');
-	for(var i=0; i<prices.length; i++){
-		var jItem = $('<li><a href="#">' + prices[i] + '</a></li>');
-		jPriceList.append(jItem);
-	}
+    var jPriceList = $('<ul/>');
+    for (var i = 0; i < prices.length; i++) {
+        var jItem = $('<li><a href="#">' + prices[i] + '</a></li>');
+        jPriceList.append(jItem);
+    }
 
-	$('#price-filter div.menu').append( jPriceList );
+    $('#price-filter div.menu').append(jPriceList);
 
-	var jThumbs = $('ul.thumbs');
-	var jPager = jThumbs.parent().find('ul.thumb-nav');
-	jThumbs.each(
-		function() {
-			var jThis = $(this);
-			var jParent = jThis.parent();
-			var jPager = jParent.find('ul.thumb-nav');
-			var jNext = jParent.find('div.next');
-			var jPrev = jParent.find('div.prev');
-			jThis.cycle({
-				fx: 'scrollHorz',
-				speed: 500,
-			    pager: jPager,
-				pagerAnchorBuilder: function(index, DOMelement){
-					return '<li><a href=""></a></li>';
-				},
-				timeout: 0,
-				activePagerClass: 'active',
-				pagerClick: function(zeroBasedSlideIndex, slideElement){},
-				pagerEvent: 'click.cycle',
+    var jThumbs = $('ul.thumbs');
+    var jPager = jThumbs.parent().find('ul.thumb-nav');
+    jThumbs.each(
+		function () {
+		    var jThis = $(this);
+		    var jParent = jThis.parent();
+		    var jPager = jParent.find('ul.thumb-nav');
+		    var jNext = jParent.find('div.next');
+		    var jPrev = jParent.find('div.prev');
+		    jThis.cycle({
+		        fx: 'scrollHorz',
+		        speed: 500,
+		        pager: jPager,
+		        pagerAnchorBuilder: function (index, DOMelement) {
+		            return '<li><a href=""></a></li>';
+		        },
+		        timeout: 0,
+		        activePagerClass: 'active',
+		        pagerClick: function (zeroBasedSlideIndex, slideElement) { },
+		        pagerEvent: 'click.cycle',
 		        next: jNext,
-	    	    prev: jPrev
+		        prev: jPrev
 
-			});
-
+		    });
 		}
 	);
+
+    $('#themes-panel').delegate('a.showFeatures', 'hover', function () {
+        var jTrigger = $(this),
+            jFeature = jTrigger.parent(),
+            jList = jFeature.find('ul');
+        if (jList.is(':visible')) {
+            jList.slideUp();
+        } else {
+            jList.slideDown();
+        }
+    });
+
 });
