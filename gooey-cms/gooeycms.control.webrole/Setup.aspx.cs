11a959da60d1b5569bd46dd1855694de32fa2c4e
cadams500 <cadams@prayer-warrior.net>
2010-10-11 20:14:54 -0500

    Add debug page to reset password

--- gooey-cms/gooeycms.control.webrole/Setup.aspx.cs
+++ gooey-cms/gooeycms.control.webrole/Setup.aspx.cs
@@ -16,17 +16,45 @@ namespace Gooeycms.Webrole.Control
             if (!Request["key"].Equals("gooeycms135abcde"))
                 throw new ApplicationException("The specified key is not valid.");
 
-            //Setup the ASP.NET Membership roles
-            if (!Roles.RoleExists(SecurityConstants.GLOBAL_ADMIN))
-                Roles.CreateRole(SecurityConstants.GLOBAL_ADMIN);
-            if (!Roles.RoleExists(SecurityConstants.DOMAIN_ADMIN))
-                Roles.CreateRole(SecurityConstants.DOMAIN_ADMIN);
-
-            String username = Request["user"];
-            if (!String.IsNullOrEmpty(username))
+            String action = Request.QueryString["a"];
+            if ("pwd".Equals(action))
             {
-                Roles.AddUserToRole(username, SecurityConstants.GLOBAL_ADMIN);
-                Response.Write("Associated: " + username + " to global admin role");
+                String username = Request.QueryString["user"];
+                String pwd = Request.QueryString["pwd"];
+
+                MembershipUser user = Membership.GetUser(username);
+                if (user != null)
+                {
+                    String password = user.ResetPassword();
+                    bool result = user.ChangePassword(password, pwd);
+                    if (result)
+                    {
+                        Response.Write("Password successfully changed");
+                    }
+                    else
+                    {
+                        Response.Write("Failed to change password");
+                    }
+                }
+                else
+                {
+                    Response.Write("Failed to find user: " + username);
+                }
+            }
+            else
+            {
+                //Setup the ASP.NET Membership roles
+                if (!Roles.RoleExists(SecurityConstants.GLOBAL_ADMIN))
+                    Roles.CreateRole(SecurityConstants.GLOBAL_ADMIN);
+                if (!Roles.RoleExists(SecurityConstants.DOMAIN_ADMIN))
+                    Roles.CreateRole(SecurityConstants.DOMAIN_ADMIN);
+
+                String username = Request["user"];
+                if (!String.IsNullOrEmpty(username))
+                {
+                    Roles.AddUserToRole(username, SecurityConstants.GLOBAL_ADMIN);
+                    Response.Write("Associated: " + username + " to global admin role");
+                }
             }
         }
     }
