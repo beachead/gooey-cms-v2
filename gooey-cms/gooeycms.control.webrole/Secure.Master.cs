b1d96a8d4dd16139bd9ffdfb02da459cf4e3af5a
Carlos Mossman <carlosmossman@gmail.com>
2010-10-27 03:31:15 -0400

    adding active state management to primary nav

--- gooey-cms/gooeycms.control.webrole/Secure.Master.cs
+++ gooey-cms/gooeycms.control.webrole/Secure.Master.cs
@@ -11,17 +11,8 @@ namespace Gooeycms.Webrole.Control
 {
     public partial class Secure : System.Web.UI.MasterPage
     {
-        protected String[] NavigationOn = new String[8] { "", "", "", "", "", "", "", "" };
         protected static String Root = null;
-        public enum NavigationType
-        {
-            Pages,
-            Site,
-            Content,
-            Campaigns,
-            Promotion,
-            Themes
-        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!CurrentSite.IsSet)
@@ -38,36 +29,19 @@ namespace Gooeycms.Webrole.Control
             Response.Redirect("~/login.aspx");
         }
 
-        public void SetNavigationOn(NavigationType type)
-        {
-            switch (type)
-            {
-                case NavigationType.Content:
-                    this.NavigationOn[1] = "on";
-                    break;
-                case NavigationType.Pages:
-                    this.NavigationOn[2] = "on";
-                    break;
-                case NavigationType.Site:
-                    this.NavigationOn[4] = "on";
-                    break;
-                case NavigationType.Campaigns:
-                    this.NavigationOn[3] = "on";
-                    break;
-                case NavigationType.Promotion:
-                    this.NavigationOn[6] = "on";
-                    break;
-                 case NavigationType.Themes:
-                    this.NavigationOn[5] = "on";
-                    break;
-                default:
-                    break;
-            }
-        }
-
         public void SetTitle(String title)
         {
             Page.Header.Title = String.Format(Page.Header.Title, title);
+        }
+
+        protected void SetActive(object sender, EventArgs e)
+        {
+            HyperLink A = (HyperLink)sender;
+            String Url = Request.Url.ToString();
+            if (Url.Contains("/" + A.ID.Replace("Nav","").ToLower() + "/"))
+            {
+                A.Attributes.Add("class", "active");
+            }
         }
     }
 }
