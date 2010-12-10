3727894f8acc41b6c41615e403b159ee099d0a2b
cadams500 <cadams@prayer-warrior.net>
2010-12-10 11:42:40 -0600

    Added ability to add global content types.
    Still need to work on fine-tuning some of the functionality, cleaning up the design, etc.
    
    bugzid: 15

--- gooey-cms/gooeycms.control.webrole/SecureGlobalAdmin.Master.designer.cs
+++ gooey-cms/gooeycms.control.webrole/SecureGlobalAdmin.Master.designer.cs
@@ -22,6 +22,15 @@ namespace Gooeycms.Webrole.Control {
         protected global::System.Web.UI.WebControls.HyperLink NavDefault;
         
         /// <summary>
+        /// HyperLink2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink2;
+        
+        /// <summary>
         /// NavDeveloper control.
         /// </summary>
         /// <remarks>
