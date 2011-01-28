e25cf4e86dfe22eb5e36cebb282889771ba9e72b
cadams500 <cadams@prayer-warrior.net>
2011-01-27 22:59:51 -0600

    Update the invite site for the new database server.
    Update the invite site for azure cross-site cookies

--- gooey-cms/gooeycms.webrole.betasignup/WebRole.cs
+++ gooey-cms/gooeycms.webrole.betasignup/WebRole.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Microsoft.WindowsAzure;
 using Microsoft.WindowsAzure.Diagnostics;
 using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.Web.Administration;
 
 namespace gooeycms.webrole.betasignup
 {
@@ -16,6 +17,34 @@ namespace gooeycms.webrole.betasignup
             // For information on handling configuration changes
             // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
             RoleEnvironment.Changing += RoleEnvironmentChanging;
+
+            /*
+             * HACK: Microsoft screwed up the machine key implementation in Azure 1.3
+             * therefore, we need to override the configuration and force the machine key
+             * The below code does just that
+             * http://msdn.microsoft.com/en-us/library/gg494983.aspx
+             */
+            using (var server = new ServerManager())
+            {
+                // This name must match the site name as defined in the ServiceDefinition.csdef
+                var siteNameFromServiceModel = "Web";
+                var siteName =
+                    string.Format("{0}_{1}", RoleEnvironment.CurrentRoleInstance.Id, siteNameFromServiceModel);
+                var siteConfig = server.Sites[siteName].GetWebConfiguration();
+
+                // get the appSettings section
+                var appSettings = siteConfig.GetSection("appSettings").GetCollection()
+                    .ToDictionary(e => (string)e["key"], e => (string)e["value"]);
+
+                // reconfigure the machine key
+                var machineKeySection = siteConfig.GetSection("system.web/machineKey");
+                machineKeySection.SetAttributeValue("validationKey", appSettings["validationKey"]);
+                machineKeySection.SetAttributeValue("validation", appSettings["validation"]);
+                machineKeySection.SetAttributeValue("decryptionKey", appSettings["decryptionKey"]);
+                machineKeySection.SetAttributeValue("decryption", appSettings["decryption"]);
+
+                server.CommitChanges();
+            }
 
             return base.OnStart();
         }
---
+++ gooey-cms/gooeycms.webrole.betasignup/auth/bulk.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="bulk.aspx.cs" Inherits="gooeycms.webrole.betasignup.auth.bulk" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>GooeyCMS Invite Manager</title>
+    <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/dojo/1.5/dijit/themes/claro/claro.css" />
+    <script src="http://ajax.googleapis.com/ajax/libs/dojo/1.5/dojo/dojo.xd.js" djConfig="parseOnLoad: true" type="text/javascript"></script>
+    <script type="text/javascript" language="javascript">
+        dojo.require("dojo.parser");
+        dojo.require("dijit.TitlePane");
+        dojo.require("dijit.dijit");
+        dojo.require("dijit.Dialog");
+        dojo.require("dijit.layout.TabContainer");
+        dojo.require("dijit.layout.ContentPane");
+        dojo.require("dijit.form.ValidationTextBox");
+        dojo.require("dijit.Tooltip");
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <b>Bulk Invite Generator</b>
+    </div>
+    </form>
+</body>
+</html>
