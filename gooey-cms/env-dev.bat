2f3465b3d52d40779df0de9d3bfa158edb520757
cadams500 <cadams@prayer-warrior.net>
2011-02-23 09:30:45 -0600

    Create simple scripts to swap between development configuration and production configuration.
    Use of this script requires that you have to hosts file, one named hosts.prod and another hosts.dev

---
+++ gooey-cms/env-dev.bat
@@ -0,0 +1,5 @@
+copy c:\windows\system32\drivers\etc\hosts.dev c:\windows\system32\drivers\etc\hosts
+copy D:\development\gooeycms\gooey-cms\gooeycms.control.webrole\Web.Debug.Config D:\development\gooeycms\gooey-cms\gooeycms.control.webrole\Web.Config
+copy D:\development\gooeycms\gooey-cms\gooeycms.webrole.ecommerce\Web.Debug.Config D:\development\gooeycms\gooey-cms\gooeycms.webrole.ecommerce\Web.Config
+copy D:\development\gooeycms\gooey-cms\gooeycms.site.webrole\Web.Debug.Config D:\development\gooeycms\gooey-cms\gooeycms.site.webrole\Web.Config
+
---
+++ gooey-cms/env-prod.bat
@@ -0,0 +1,4 @@
+copy c:\windows\system32\drivers\etc\hosts.prod c:\windows\system32\drivers\etc\hosts
+copy D:\development\gooeycms\gooey-cms\gooeycms.control.webrole\Web.Prod.Config D:\development\gooeycms\gooey-cms\gooeycms.control.webrole\Web.Config
+copy D:\development\gooeycms\gooey-cms\gooeycms.webrole.ecommerce\Web.Prod.Config D:\development\gooeycms\gooey-cms\gooeycms.webrole.ecommerce\Web.Config
+copy D:\development\gooeycms\gooey-cms\gooeycms.site.webrole\Web.Prod.Config D:\development\gooeycms\gooey-cms\gooeycms.site.webrole\Web.Config
